30fefa45b54ac54476531dffbacc7614cb898630
Fabiano F. P. Stoffel <ffpthai@gmail.com>
2021-02-07 17:23:29 -0300

    Nível 04 | Organizando o container

--- nivel04/Arquitetura_e_testes_no_Node.js/src/shared/container/providers/MailProvider/index.ts
+++ nivel04/Arquitetura_e_testes_no_Node.js/src/shared/container/providers/MailProvider/index.ts
@@ -1,7 +1,16 @@
+import { container } from 'tsyringe';
+
+import mailConfig from '@config/mail';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailprovider';
 import EtherealMailPorvider from './implementations/EtherealMailProvider';
 import SESMailProvider from './implementations/SESMailProvider';
 
-export default {
-  ethereal: EtherealMailPorvider,
-  ses: SESMailProvider,
+const providers = {
+  ethereal: container.resolve(EtherealMailPorvider),
+  ses: container.resolve(SESMailProvider),
 };
+
+container.registerInstance<IMailProvider>(
+  'RgIMailProvider',
+  providers[mailConfig.driver],
+);
--- nivel04/Arquitetura_e_testes_no_Node.js/src/shared/container/providers/index.ts
+++ nivel04/Arquitetura_e_testes_no_Node.js/src/shared/container/providers/index.ts
@@ -1,34 +1,3 @@
-import { container } from 'tsyringe';
-
-import mailConfig from '@config/mail';
-
-import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
-import IMailProvider from '@shared/container/providers/MailProvider/models/IMailprovider';
-import EtherealMailPorvider from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
-import SESMailProvider from '@shared/container/providers/MailProvider/implementations/SESMailProvider';
-import IMailtemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailtemplateProvider';
-import HandlebarsMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'RgStorageProvider',
-  DiskStorageProvider,
-);
-
-container.registerSingleton<IMailtemplateProvider>(
-  'RgMailTemplateProvider',
-  HandlebarsMailTemplateProvider,
-);
-
-const containerResolve = {
-  ses: () => container.resolve(SESMailProvider),
-  ethereal: () => container.resolve(EtherealMailPorvider),
-};
-
-container.registerInstance<IMailProvider>(
-  'RgIMailProvider',
-  containerResolve[mailConfig.driver](),
-  // mailConfig.driver === 'ethereal'
-  //   ? container.resolve(EtherealMailPorvider)
-  //   : container.resolve(SESMailProvider),
-);
+import '@shared/container/providers/StorageProvider';
+import '@shared/container/providers/MailTemplateProvider';
+import '@shared/container/providers/MailProvider';
