3b62b246aa468aeb209aeb25a1c7e88587407a3e
Fabiano F. P. Stoffel <ffpthai@gmail.com>
2021-03-08 19:11:07 -0300

    Nível 05 | Tela de Sucesso

--- nivel05/appgobarber/src/hooks/Auth.tsx
+++ nivel05/appgobarber/src/hooks/Auth.tsx
@@ -67,7 +67,7 @@ export const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
-    api.defaults.headers.authorization = `Bearer ${token[1]}`;
+    api.defaults.headers.authorization = `Bearer ${token}`;
     setData({ token, user });
   }, []);
 
