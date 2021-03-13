a6aaa38ad904bf18fcf0ad71c5885c7b45fcb34a
Fabiano F. P. Stoffel <ffpthai@gmail.com>
2021-03-12 21:45:02 -0300

    Nível 05 | Teste de login

--- nivel05/gobarber-web/src/__tests__/pages/Signin.spec.tsx
+++ nivel05/gobarber-web/src/__tests__/pages/Signin.spec.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Signin from '../../pages/Signin';
+
+const mockedHostoryPush = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedHostoryPush,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
 describe('Signin Page', () => {
   it('should be able to sign in', () => {
-    const { debug } = render(<Signin />);
-    debug();
+    // const { debug } = render(<Signin />);
+    // debug();
+    const { getByPlaceholderText, getByText } = render(<Signin />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordFiels = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, { target: { value: 'b@b.com' } });
+    fireEvent.change(passwordFiels, { target: { value: '123456' } });
+    fireEvent.click(buttonElement);
+
+    expect(mockedHostoryPush).toHaveBeenCalledWith('/dashboard');
   });
 });
